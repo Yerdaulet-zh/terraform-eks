52a600fa7bf11caed8a4b8cb9b965e8c4966245a
Yerdaulet-zh <yerdaulet.zhumabay@gmail.com>
2025-05-28 14:42:46 +0500

    Enamble s3 server side encryption

--- bootstrap/backend.tf
+++ bootstrap/backend.tf
@@ -4,5 +4,6 @@ terraform {
         key            = "bootstrap/terraform.tfstate"
         region         = "us-east-1"
         dynamodb_table = "terraform-state-lock-pet-project-eks"
+        encrypt        = true
     }
 }
