6802df6d04df858d346b747fad519dab62921e41
Yerdaulet-zh <yerdaulet.zhumabay@gmail.com>
2025-05-28 21:23:29 +0500

    configure backend for terraform eks

---
+++ development/eks/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+    backend "s3" {
+        bucket         = "terraform-state-pet-project-eks"
+        key            = "development/eks/terraform.tfstate"
+        region         = "us-east-1"
+        dynamodb_table = "terraform-state-lock-pet-project-eks"
+        encrypt        = true
+    }
+}
