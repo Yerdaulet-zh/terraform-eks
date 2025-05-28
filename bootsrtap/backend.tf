7242aad2f644b990d369f81af558331244304087
Yerdaulet-zh <yerdaulet.zhumabay@gmail.com>
2025-05-28 14:33:52 +0500

    Configure and setup of the project's S3 bucket and DynamoDB table

---
+++ bootsrtap/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+    backend "s3" {
+        bucket         = "terraform-state-pet-project-eks"
+        key            = "bootstrap/terraform.tfstate"
+        region         = "us-east-1"
+        dynamodb_table = "terraform-state-lock-pet-project-eks"
+    }
+}
